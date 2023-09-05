cafde4ca2e7e862494805b5732c9b31a758127d8
oussama <oussama2@cominity.ca>
2023-09-05 23:48:54 +0100

    feat: add header component

---
+++ src/infrastructure/presentation/components/Header.tsx
@@ -0,0 +1,116 @@
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    useColorMode,
+  } from '@chakra-ui/react'
+  import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Link, useNavigate } from '@tanstack/react-router'
+  
+  interface Props {
+    children: React.ReactNode
+  }
+  
+  const Links: string[] = []
+  
+  const NavLink = (props: Props) => {
+    const { children } = props
+  
+    return (
+      <Box
+        as="a"
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+          textDecoration: 'none',
+          bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+      </Box>
+    )
+  }
+  
+  export default function Header() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { colorMode, toggleColorMode } = useColorMode()
+    const navigate = useNavigate()
+  
+    return (
+      <>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={'center'}>
+              <Box>Logo</Box>
+              <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+                {Links.map((link) => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </HStack>
+            </HStack>
+            <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={
+                      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    }
+                  />
+                </MenuButton>
+                <MenuList>
+                  <Link to={'/register'}>
+                  <MenuItem>
+                    signup
+                </MenuItem>
+                  </Link>
+                  
+                  <Link to={"/login"}><MenuItem>Login</MenuItem></Link>
+                </MenuList>
+              </Menu>
+
+            </Stack>
+            
+            </Flex>
+          </Flex>
+  
+          {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {Links.map((link) => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </Stack>
+            </Box>
+          ) : null}
+        </Box>
+      </>
+    )
+  }
