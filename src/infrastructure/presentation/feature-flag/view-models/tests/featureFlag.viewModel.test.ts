4b950b540a0731fa4eb5337dda6396e7e2f6c2ca
oussama <oussama2@cominity.ca>
2023-09-05 19:02:13 +0100

    feat: test view modal of feature flag

---
+++ src/infrastructure/presentation/feature-flag/view-models/tests/featureFlag.viewModel.test.ts
@@ -0,0 +1,45 @@
+import { describe, it, beforeEach, vi, expect, Mocked } from "vitest";
+import {FeatureFlagViewModel} from "../featureFlag.viewModel"
+import { GetFeatureFlag } from "#domaine/feature-flag/use-cases/get-feature-flag/GetFeatureFlag.usecase";
+
+describe('FeatureFlagViewModel', () => {
+  let getFeatureFlagMock: Mocked<GetFeatureFlag>;
+  let viewModel: FeatureFlagViewModel;
+
+  beforeEach(() => {
+    // Initialisation des mocks
+    getFeatureFlagMock = {
+      execute: vi.fn(),
+    } as any;
+
+    viewModel = new FeatureFlagViewModel(getFeatureFlagMock);
+  });
+
+  it('should return true if feature is enabled', async () => {
+    const featureName = 'some-feature';
+    getFeatureFlagMock.execute.mockResolvedValue({ featureFlag: { isEnabled: true, name: "some-feature" } });
+
+    const result = await viewModel.isFeatureEnabled(featureName);
+
+    expect(result).toBe(true);
+    expect(getFeatureFlagMock.execute).toHaveBeenCalledWith({ featureName });
+  });
+
+  it('should return false if feature is disabled', async () => {
+    const featureName = 'some-feature';
+    getFeatureFlagMock.execute.mockResolvedValue({ featureFlag: { isEnabled: false, name: "some-feature" } });
+
+    const result = await viewModel.isFeatureEnabled(featureName);
+
+    expect(result).toBe(false);
+    expect(getFeatureFlagMock.execute).toHaveBeenCalledWith({ featureName });
+  });
+
+  it('should return false if feature name is invalid', async () => {
+    const featureName = ''; // assuming this is invalid
+    const result = await viewModel.isFeatureEnabled(featureName);
+
+    expect(result).toBe(false);
+    expect(getFeatureFlagMock.execute).not.toHaveBeenCalled();
+  });
+});
