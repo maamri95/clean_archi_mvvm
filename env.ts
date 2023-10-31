c05c4d2e4aca4000a2e5e44214f6535a16d5613c
oussama <oussama2@cominity.ca>
2023-10-31 15:06:27 +0100

    fix(env): Fix coerce method for CI variable in env.ts
    - Refactored the coerce method for the CI variable in the env.ts file.
    - Changed from `z.boolean().optional().default(false)` to `z.coerce.boolean().optional().default(false)`.
    - This change ensures that the CI variable is correctly coerced to a boolean value in the environment schema.
    - The previous method was not properly coercing the CI variable, leading to potential issues.
    - This fix ensures that the CI variable is properly handled in the environment configuration.

--- env.ts
+++ env.ts
@@ -3,5 +3,5 @@ import z from "zod";
 export const envSchema = z.object({
     NXT_API_BASE_URL: z.string().url("Must be a valid URL").transform((value) => new URL(value)),
     NODE_ENV: z.enum(["development", "production", "test"]).optional().default("development"),
-    CI: z.boolean().optional().default(false),
+    CI: z.coerce.boolean().optional().default(false),
 });
