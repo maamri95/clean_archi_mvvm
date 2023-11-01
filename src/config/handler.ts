371401b2effc990829395afe9721b3b08eeee600
oussama <oussama2@cominity.ca>
2023-11-01 19:53:13 +0100

    fix(core): logger dependency injection

--- src/config/handler.ts
+++ src/config/handler.ts
@@ -1,5 +1,6 @@
 import DependencyContainer from "tsyringe/dist/typings/types/dependency-container";
 import {GlobalErrorHandler} from "#infrastructure/errorHandler/globalErrorHandler.ts";
+import {Env} from "#utils/env.ts";
 
 /**
  * Setup error handler for client application
@@ -9,7 +10,7 @@ export const setupErrorHandler = (container: DependencyContainer) => {
     const globalErrorHandler = container.resolve<GlobalErrorHandler>(GlobalErrorHandler);
     globalThis.onerror = (message, _source, _lineno, _colno, error) => {
         globalErrorHandler.execute(error, message.toString());
-        return false;
+        return Env("NODE_ENV") === "production";
     };
     globalThis.onunhandledrejection = (error) => {
         globalErrorHandler.execute(error, error.reason);
--- src/infrastructure/errorHandler/globalErrorHandler.ts
+++ src/infrastructure/errorHandler/globalErrorHandler.ts
@@ -7,12 +7,23 @@ export class GlobalErrorHandler extends ErrorHandler {
     constructor(@inject(DI_TOKENS.logger) private logger: Logger) {
         super();
     }
+
+    /**
+     * Handle an error send only in development
+     * @param error
+     * @param message
+     */
     handle(error: any, message: string): void {
         this.logger.error(message, error);
     }
 
-    report(_error: any, _message: string): void {
-
+    /**
+     * Report an error send only in production
+     * @param error
+     * @param message
+     */
+    report(error: any, message: string): void {
+        this.logger.error(message, error);
     }
 
 }
